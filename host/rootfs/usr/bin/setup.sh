3e72e09751cf5fd14054fccef606db869760c667
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2022-10-21 14:13:20 +0300

    Set up correct resolutions
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- host/rootfs/usr/bin/setup.sh
+++ host/rootfs/usr/bin/setup.sh
@@ -4,27 +4,22 @@ export WAYLAND_DISPLAY=wayland-1
 export XDG_RUNTIME_DIR=/run/user/0
 export WESTON_CONFIG_FILE=/etc/xdg/weston/weston.ini
 
-export MESA_DEBUG=1
-export EGL_LOG_LEVEL=debug
-# export LIBGL_DEBUG=verbose
-# export WAYLAND_DEBUG=1
-
-export ELECTRON_ENABLE_LOGGING=true
-export ELECTRON_DEBUG_NOTIFICATIONS=true
-export ELECTRON_ENABLE_STACK_DUMPING=true
-
+echo "12345678901264758695847364857456" > /etc/machine-id
 mkdir -p /home
 adduser ivan -D -h /home/ivan
 chown ivan:ivan /home/ivan
+chown -R ivan /run/user/0
+chown ivan /dev/pts/0
 mkdir /tmp && chmod 0777 /tmp
 chmod 666 /dev/null
-chown -R ivan:ivan /run/user/0
-chmod -R 0755 /run/user/0
-echo "12345678901264758695847364857456" > /etc/machine-id
+chmod 666 /dev/urandom
+chmod 666 /dev/tty
 chmod 666 /dev/dri/card0
 chmod 666 /dev/dri/renderD128
 
 
-# /nix/store//bin/electron --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox --ignore-gpu-blacklist
+# /nix/store/npknxl8a5lnc451pj3c2sqbpl5qdri5a-electron-19.0.7/bin/electron --enable-features=UseOzonePlatform --ozone-platform=wayland
 
-# dbus-launch element-desktop  --enable-features=UseOzonePlatform --ozone-platform=wayland --ignore-gpu-blacklist
+# chromium --enable-features=UseOzonePlatform --ozone-platform=wayland
+
+# NIXOS_OZONE_WL=1 /nix/store/xs78zrfqcpy3izj19cg5iszy31lqza0d-element-desktop-1.11.5/bin/element-desktop --enable-features=UseOzonePlatform --ozone-platform=wayland
